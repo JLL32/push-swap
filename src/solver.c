8f6ad07fc7da584dbae802fe9d1a9af8f7a8d493
ael-hach <ael-hach@e1r3p10.1337.ma>
2021-06-06 17:03:25 +0100

    get_next_slice() is now completed, tested and ready to work

--- src/solver.c
+++ src/solver.c
@@ -3,33 +3,48 @@
 #include <stdlib.h>
 #include <sys/_types/_size_t.h>
 
-void solve(t_stack *stack_a, t_stack *stack_b, int *input) {
-	sort(input);
-	push_chunks(stack_a, stack_b, input);
-	send_all(stack_b, stack_a);
-}
+/* void solve(t_stack *stack_a, t_stack *stack_b, int *input) { */
+/* 	sort(input); */
+/* 	push_chunks(stack_a, stack_b, input); */
+/* 	send_all(stack_b, stack_a); */
+/* } */
 
-void push_chunks(t_stack *stack_src, t_stack *stack_dst, int *sorted_arr) {
-	int *chunk;
-	size_t l;
-	size_t chunk_l;
+/* void push_chunks(t_stack *stack_src, t_stack *stack_dst, int *sorted_arr) { */
+/* 	t_slice *chunk; */
+/* 	size_t l; */
+/* 	size_t chunk_l; */
 
-	l =  stack_src->length;
-	chunk_l = chunk_l <= 150 ? 15 : 39;
-	while ((chunk = get_next_slice(sorted_arr, l, chunk_l))) {
-		push_chunk(stack_src, stack_dst, chunk);
+/* 	l =  stack_src->length; */
+/* 	chunk_l = chunk_l <= 150 ? 15 : 39; */
+/* 	while ((chunk = get_next_slice(sorted_arr, l, chunk_l))) { */
+/* 		push_chunk(stack_src, stack_dst, chunk); */
+/* 		free(chunk); */
+/* 	} */
+/* } */
+
+/*
+** NOTE: t_slice.end is exclusive
+** NOTE: don't forget to free t_slice
+*/
+
+t_slice *get_next_slice(int *arr, size_t size, size_t max_chunk_size) {
+	t_slice *slice;
+	static size_t global_i;
+	size_t local_i;
+
+	slice = malloc(sizeof(t_slice));
+	slice->data = arr;
+	slice->start = global_i;
+	slice->end = global_i;
+	local_i = 0;
+	while (global_i < size) {
+		slice->end++;
+		global_i++;
+		local_i++;
+		if (local_i == max_chunk_size || global_i == size) {
+			return (slice);
+		}
 	}
-}
-
-int *get_next_slice(int *arr, size_t size, size_t max_chunk_size) {
-	int *chunk;
-	static size_t arr_l;
-	size_t start_i;
-
-	chunk = malloc(sizeof(int));
-	arr_l = size;
-	start_i = 0;
-	while (arr_l && ) {
-	statements
-	}
+	free(slice);
+	return (NULL);
 }
--- src/solver.h
+++ src/solver.h
@@ -2,12 +2,18 @@
 #include <malloc/_malloc.h>
 #include <sys/_types/_size_t.h>
 
+typedef struct s_slice {
+	const int *data;
+	size_t start;
+	size_t end;
+} t_slice;
+
 void solve(t_stack *stack_a, t_stack *stack_b, int *input);
 void send_from_top(t_stack *stack_src, t_stack *stack_dst, int value);
 void send_from_bottom(t_stack *stack_src, t_stack *stack_dst, int value);
 void send_biggest(t_stack *stack_src, t_stack *stack_dst);
 void send_all(t_stack *stack_src, t_stack *stack_dst);
-int *get_next_slice(int *arr, size_t size, size_t max_chunk_size);
-void push_chunk(t_stack *stack_src, t_stack *stack_dst, int *chunk);
+t_slice *get_next_slice(int *arr, size_t size, size_t max_chunk_size);
+void push_chunk(t_stack *stack_src, t_stack *stack_dst, t_slice *chunk);
 void push_chunks(t_stack *stack_src, t_stack *stack_dst, int *sorted_arr);
 void sort(int *arr);
