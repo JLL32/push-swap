71f4a748fd47640247b79c3751172a073e23d2b6
Tahabz <etahabaz@gmail.com>
2021-06-11 17:57:29 +0100

    adds print_array()

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include "solver.h"
 #include "input-utils.h"
 
+void print_array(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d\n", arr[i]);
+}
 
 // TODO: check if sorted first
 int main(int argc, char **argv) {
@@ -13,10 +19,12 @@ int main(int argc, char **argv) {
 	stack_a = new_stack("a");
 	stack_b = new_stack("b");
 	fill_stack(stack_a, input, argc - 1);
-	/* print_stack(stack_a, true); */
+	//print_stack(stack_a, true);
 	solve(stack_a, stack_b, input);
-	/* print_stack(stack_a, true); */
-	/* print_stack(stack_b, true); */
+	//print_stack(stack_a, true);
+	// print_stack(stack_b, true);
+	//sort(input, argc-1);
+	//printArray(input, argc-1);
 	delete_stack(stack_a);
 	delete_stack(stack_b);
 	free(input);
