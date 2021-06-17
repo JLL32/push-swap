32bc3ea92484b1823251886769fcb2d6939fa470
Tahabz <etahabaz@gmail.com>
2021-06-17 14:53:22 +0100

    free memory and fix the norm (not yet completed)

--- src/checker.c
+++ src/checker.c
@@ -2,67 +2,94 @@
 #include "get_next_line/get_next_line.h"
 #include "input-utils.h"
 
-int execute_ops(char **ops, t_stack *stack_a, t_stack *stack_b)
+int	execute_ops(char **ops, t_stack *stack_a, t_stack *stack_b)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (ops[i])
-    {
-        if (strcmp(ops[i], "ra") == 0)
-            rotate(stack_a);
-        else if (strcmp(ops[i], "rb") == 0)
-            rotate(stack_b);
-        else if (strcmp(ops[i], "rra") == 0)
-            reverse_rotate(stack_a);
-        else if (strcmp(ops[i], "rrb") == 0)
-            reverse_rotate(stack_b);
-        else if (strcmp(ops[i], "sa") == 0)
-            swap(stack_a);
-        else if (strcmp(ops[i], "sb") == 0)
-            swap(stack_b);
-        else if (strcmp(ops[i], "pa") == 0)
-            send(stack_b, stack_a);
-        else if (strcmp(ops[i], "pb") == 0)
-            send(stack_a, stack_b);
-        else
-            return -1;
-        i++;
-    }
-    return 1;
+	i = 0;
+	while (ops[i])
+	{
+		if (strcmp(ops[i], "ra") == 0)
+			rotate(stack_a);
+		else if (strcmp(ops[i], "rb") == 0)
+			rotate(stack_b);
+		else if (strcmp(ops[i], "rra") == 0)
+			reverse_rotate(stack_a);
+		else if (strcmp(ops[i], "rrb") == 0)
+			reverse_rotate(stack_b);
+		else if (strcmp(ops[i], "sa") == 0)
+			swap(stack_a);
+		else if (strcmp(ops[i], "sb") == 0)
+			swap(stack_b);
+		else if (strcmp(ops[i], "pa") == 0)
+			send(stack_b, stack_a);
+		else if (strcmp(ops[i], "pb") == 0)
+			send(stack_a, stack_b);
+		else
+			return (-1);
+		i++;
+	}
+	return (1);
 }
 
-int main(int argc, char **argv)
+void	free_stack(t_stack *stack)
 {
-    char *line;
-    char **ops;
-    int i;
+	t_node	*curr_node;
 
-    i = 0;
-    // you might ask why i moved the statement below to the top
-    // try to figure it out on your own ;)
-    int *input = input_from_arg(argc - 1, argv);
-    t_stack *stack_a = new_stack("a");
-    t_stack *stack_b = new_stack("b");
-    fill_stack(stack_a, input, argc - 1);
-    print_stack(stack_a, false);
-    ops = malloc(sizeof(*ops));
-    while (get_next_line(0, &line) > 0)
-    {
-        ops[i] = line;
-        i++;
-        ops = realloc(ops, i * sizeof(*ops) + sizeof(*ops));
-    }
-    ops[i] = NULL;
-    free(line);
-    if (execute_ops(ops, stack_a, stack_b) == -1)
-    {
-        write(2, "Error\n", 6);
-        // free_memory(stack_a, stack_b, ops, arr);
-    }
-    if (is_sorted(stack_a))
-        write(1, "OK", 2);
-    else
-        write(1, "KO", 2);
-    return 0;
+	curr_node = stack->bottom;
+	while (curr_node)
+	{
+		free(curr_node);
+		curr_node = curr_node->next;
+	}
+	free(stack);
+}
+
+void	free_memory(t_stack *stack_a, t_stack *stack_b, char **ops)
+{
+	int	i;
+
+	i = 0;
+	free_stack(stack_a);
+	free_stack(stack_b);
+	while (ops[i])
+	{
+		free(ops[i]);
+		i++;
+	}
+	free(ops);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*line;
+	char	**ops;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	int		i;
+
+	i = 0;
+	stack_a = new_stack("a");
+	stack_b = new_stack("b");
+	fill_stack(stack_a, input_from_arg(argc - 1, argv), argc - 1);
+	print_stack(stack_a, false);
+	ops = malloc(sizeof(*ops));
+	while (get_next_line(0, &line) > 0)
+	{
+		ops[i] = line;
+		i++;
+		ops = realloc(ops, i * sizeof(*ops) + sizeof(*ops));
+	}
+	ops[i] = NULL;
+	free(line);
+	if (execute_ops(ops, stack_a, stack_b) == -1)
+	{
+		write(2, "Error\n", 6);
+		free_memory(stack_a, stack_b, ops);
+	}
+	if (is_sorted(stack_a))
+		write(1, "OK", 2);
+	else
+		write(1, "KO", 2);
+	return (0);
 }
